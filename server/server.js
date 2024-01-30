f52626abc31fa2e7cd637a8ce98436bf1345ff3a
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2024-01-30 15:01:49 -0500

    adds port info to server.js, GET planets is displaying test data on the DOM

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const data = require("./data_access");
 
 const app = express();
@@ -7,13 +7,24 @@ const app = express();
 app.use(bodyParser.json());
 
 // GET planets
-app.get("/planets/:id", function(req, res) {
-    let planet = data.findPlanet(req.params.id);
+app.get("/planets/:id", function (req, res) {
+  // let planet = data.findPlanet(req.params.id);
 
-    if(planet === undefined) {
-        res.statusCode = 404;
-        res.end();
-    } else {
-        res.send(planet);
-    }
+  let planet = {
+    id: 1,
+    name: "test planet",
+    climate: "desert",
+    temperate: "arid",
+  };
+
+  if (planet === undefined) {
+    res.statusCode = 404;
+    res.end();
+  } else {
+    res.send(planet);
+  }
 });
+
+var port = 3000;
+console.log("service opening on port: " + port);
+app.listen(port);
